9f92d69ab447aba9280f721edc95e9a1636c3dd4
simon <45198485+swagasoft@users.noreply.github.com>
2019-05-08 00:29:27 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,7 @@
 export const environment = {
   production: false,
    firebase : {
-    apiKey: 'AIzaSyAcBK8zXb6qt8BaHD_i-KdWZkdi1WQVnM0',
-    authDomain: 'moneymine-76c9e.firebaseapp.com',
-    databaseURL: 'https://moneymine-76c9e.firebaseio.com',
-    projectId: 'moneymine-76c9e',
-    storageBucket: 'moneymine-76c9e.appspot.com',
-    messagingSenderId: '808687065388',
-    appId: '1:808687065388:web:4b0e43773aae95e0'
+ 
   }
 };
 
